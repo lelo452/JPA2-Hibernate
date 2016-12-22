ef8b10a80a4d0eda416f6b8539a8f6ee888b3cc6
lelo452 <leonardo_c.a.p2@hotmail.com>
2016-12-22 15:20:49 -0200

    First Commit

---
+++ src/main/java/br/com/caelum/model/Loja.java
@@ -0,0 +1,37 @@
+package br.com.caelum.model;
+
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Loja {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	// SpringMVC usa pra passar o id
+	@Deprecated
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
